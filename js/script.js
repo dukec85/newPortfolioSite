b8e89aee8a8bedc21a402f4d0a5b052e882005f1
Christopher Duke <cd.duke@gmail.com>
2021-02-07 18:40:20 -0600

    added copyright section and minor bug fix

--- js/script.js
+++ js/script.js
@@ -1,67 +1,97 @@
 $(document).ready(function() {
-    $('#slides').superslides({
-        animations: 'fade',
-        play: 5000,
-        pagination: false
-    });
 
-    var typed = new Typed(".typed", {
-        strings: ["Software Engineer", "Web Developer"],
-        typeSpeed: 70,
-        loop: true,
-        startDelay: 1000,
-        showCursor: false
-    });
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        items: 4,
-        responsive:{
-            0:{
-                items:1
-            },
-            480:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            938:{
-                items:4
-            }
-        }
-    });
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
 
 
-        var skillsTopOffset = $(".skillsSection").offset().top;
 
-        $(window).scroll(function() {
 
-            if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-                $('.chart').easyPieChart({
-                        easing: 'easeInOut',
-                        barColor: '#fff',
-                        trackColor: false,
-                        scaleColor: false,
-                        lineWidth: 4,
-                        size: 152,
-                        onStep: function(from, to, percent) {
-                            $(this.el).find('.percent').text(Math.round(percent));
-                        }
-                    });
-            }
 
-        });
+	var skillsTopOffset = $(".skillsSection").offset().top;
 
-        $("[data-fancybox]").fancybox();
+	$(window).scroll(function() {
 
-        $(".items").isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 1500,
-                easing: 'linear',
-                queue: false
-            }
-        });
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+
+
+		}
+
+
+	});
+
+
+	$("[data-fancybox]").fancybox();
+
+
+	$(".items").isotope({
+		filter: '*',
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+
+		return false;
+	});
+
 
 });
